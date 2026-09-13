d238747f6a600a7ee499cc91564305067afefc24
Nishant <bhatinishant38@gmail.com>
2026-09-13 22:44:34 +0530

    Add build scripts and generated TypeScript declaration files

---
+++ dist/index.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+function greet(name) {
+    return ` Hello ${name}`;
+}
+const userName = "Nishant";
+console.log(greet(userName));
+//# sourceMappingURL=index.js.map
