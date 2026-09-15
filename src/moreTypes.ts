0299bea46318cd4377200ad34ed44a53391c750b
Nishant <bhatinishant38@gmail.com>
2026-09-16 00:01:22 +0530

    Add try-catch error handling and implement role-based redirection logic

--- src/moreTypes.ts
+++ src/moreTypes.ts
@@ -38,3 +38,38 @@
  if(typeof newValue === "number"){
     console.log(newValue)
  }
+
+
+ // try catch
+
+ try {
+    
+ } catch (error) {
+    if(error instanceof Error){
+        console.log(error.message)
+    }
+    console.log('Error',error)
+ }
+
+ const data : unknown = "chai and code"
+ const strData : string = data as string
+
+ // Never data types
+
+ type Role = "admin"|"user" | "superadmin"
+
+ function redirectBasedOnRole(role: Role):void{
+    if(role==='admin'){
+        console.log('Redirecting to admin dashboard')
+        return
+    }
+    if(role==='user'){
+         console.log('Redirecting to user dashboard')
+         return
+    }
+    role
+ }
+
+ function neverReturn():never{
+    while(true){}
+ }
