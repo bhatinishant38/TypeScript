d21d9a40f36c2e544af150a0f61beefad609d3e3
Nishant <bhatinishant38@gmail.com>
2026-09-16 23:06:14 +0530

    Add functions for chai preparation and order management

---
+++ src/function.Ts.ts
@@ -0,0 +1,35 @@
+function makeChai(type :string ,cups : number){
+    console.log(`Making ${cups} cups of ${type}`)
+}
+
+makeChai('masala', 2)
+
+function getChaiPrice():number{
+    return 25
+}
+
+function makeOrder(order : string) {
+    if(!order) return null
+    return order
+}
+
+function logChai():void{
+    console.log('Chai is ready')
+}
+
+// function orderChai(type ?: string){
+
+// }
+
+function orderChai(type :string = 'masala'){
+
+}
+
+function createChai (order :{
+    type :string
+    sugar : number
+    size : 'small' | 'large'  
+}):number{
+    console.log()
+    return 34
+}
